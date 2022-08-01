88d979a9eb7535a96311634befdf9f5ed2fe4959
hasanshahrear <hasanshahrear143@gmail.com>
2022-08-01 21:08:05 +0600

    Project setup

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className='bg-violet-500 text-2xl'>Hasan Shahrear</h1>
+    </div>
+  );
+}
+
+export default App;
